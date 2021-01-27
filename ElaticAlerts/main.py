bb524a0d85288fbcb1b63cd8345baa8ccb5d535c
itchief <itchief@kali.c0moshack.com>
2021-01-27 11:21:00 -0600

    Added jsonpath_rw to allow for displaying the filtered field in the output

--- ElaticAlerts/main.py
+++ ElaticAlerts/main.py
@@ -3,26 +3,28 @@
 import smtplib, socket
 import yaml, json, requests, urllib3
 from requests.auth import HTTPBasicAuth
+from jsonpath_rw import parse
 #from dictor import dictor
 
-
+# Disable SSL certificate warnings in case self-signed certificates are being used
+# It is recommended to use a valid certificate
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # Import the alerts configuration
-file = open('config/test2.yml','r')
+file = open('config/test.yml','r')
 cfg = yaml.load(file, Loader=yaml.FullLoader)
     
 #def alertQuery(index_pattern,time_period,frequency, query):
-def alertQuery(index_pattern, field, term, negate) :
+def alertQuery(index_pattern, field, term,time_period, negate) :
     
     if negate == True :
         operator = "must_not"
     if negate == False :
         operator = "must"
 
-    jquery = json.loads('{  "query": { "bool": {"' + operator + '": [{"term": {"' + field + '": "' + term + '"}}],"filter":[{"exists":{"field": "' + field + '"}}]}}}')
+    jquery = json.loads('{  "query": { "bool": {"' + operator + '": [{"term": {"' + field + '": "' + term + '"}}],"filter":[{"exists":{ "field": "' + field + '" }},{ "range": { "@timestamp":{ "gte": "now-' + time_period + '" }}}]}}}')
     headers = {'Accept': 'application/json', 'Content-type': 'application/json'}
-    
+
     if cfg['elasticsearch']['ssl'] == True: 
         queryURL = "https://"  + cfg["elasticsearch"]["host"]  + ":" + cfg["elasticsearch"]["port"] + "/" + index_pattern + "/_search"
         try:
@@ -43,15 +45,18 @@ def getFieldNames(field) :
     if '.' not in field :
         return field
     else :
+        parsedField = ""
         values = field.split(".")
-        field = "['" + values[0] + "']['" + values[1] + "']"
-        return field
-
-def sendEvents(host, port, username, password, message) :
+        for v in values :
+            parsedField += "'" + v + "'"
+        
+        return parsedField
+            
+def sendEvents(host, port, username,recipient, password, message) :
     #sender = socket.getfqdn()
     
     sent_from = username
-    to = [username]
+    to = [recipient]
     subject = 'Elastic Alert Message'
     body = message
     
@@ -77,10 +82,13 @@ Subject: %s
 
 # This is where we check all configured alerts.
 for alert in cfg['alerts'] :
-    json_response = alertQuery(alert['index_pattern'],alert['field'],alert['term'],alert['negate'])
+    json_response = alertQuery(alert['index_pattern'],alert['field'],alert['term'],alert['time_period'], alert['negate'])
+    #print(json.dumps(json_response, indent=4))
     
     for hit in json_response['hits']['hits'] :
-        print(hit['_source'])
-        if cfg['output']['smtp']['enabled'] :
-            sendEvents(cfg['output']['smtp']['host'] , cfg['output']['smtp']['port'] , cfg['output']['smtp']['username'] , cfg['output']['smtp']['password'] , hit['_source'])
-        
+        #print(json.dumps(hit['_index'], indent=2))
+        output = parse(alert['field']).find(hit['_source'])
+        print(output[0].value)
+
+        #if cfg['output']['smtp']['enabled'] :
+        #    sendEvents(cfg['output']['smtp']['host'] , cfg['output']['smtp']['port'] , cfg['output']['smtp']['username'], alert['recipient'], cfg['output']['smtp']['password'] , json.dumps(hit['_source'], indent=2))        
