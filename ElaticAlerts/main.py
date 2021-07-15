4e9ae3377cf2efa5a20c81c248f70c0634d63ca4
ITChief <paulbrown4@gmail.com>
2021-07-15 14:05:38 -0500

    Modify yaml configuration parameters

--- ElaticAlerts/main.py
+++ ElaticAlerts/main.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
 
-import smtplib, time, logging, os
+import smtplib, time, logging, os, argparse, copy
 import yaml, json, requests, urllib3
 from requests.auth import HTTPBasicAuth
 from jsonpath_rw import parse
 from datetime import datetime
-#from dictor import dictor
 
 if not os.path.exists('/var/log/ElasticAlerts'):
     os.makedirs('/var/log/ElasticAlerts')
@@ -21,21 +20,31 @@ logger.setLevel(logging.DEBUG)
 # It is recommended to use a valid certificate
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# Import the alerts configuration
-file = open('/opt/ElasticAlerts/ElaticAlerts/config/test.yml','r')
-cfg = yaml.load(file, Loader=yaml.FullLoader)
+def alertQuery(index_pattern, time_period, must="", must_not="") :
     
-#def alertQuery(index_pattern,time_period,frequency, query):
-def alertQuery(index_pattern, field, term,time_period, negate) :
-    
-    if negate == True :
-        operator = "must_not"
-    if negate == False :
-        operator = "must"
+    if must :
+        must_array = ""
+        for key in must :
+            must_array += '{ "match": { "' + key.split(':')[0] + '": "' + key.split(':')[1] + '" }},'
 
-    jquery = json.loads('{  "query": { "bool": {"' + operator + '": [{"match": {"' + field + '": "' + term + '"}}],"filter":[{"exists":{ "field": "' + field + '" }},{ "range": { "@timestamp":{ "gte": "now-' + time_period + '" }}}]}}}')
+        must_array = must_array[:-1]
+        
+    if must_not : 
+        mustnot_array = ""
+        for key in must_not :
+            mustnot_array += '{ "match": { "' + key.split(':')[0] + '": "' + must_not.split(':')[1] + '" }},'
+
+        mustnot_array = mustnot_array[:-1]
+    
+    if must_array in locals() and mustnot_array in locals() :
+        jquery = json.loads('{  "query": { "bool": {"must": [' + must_array + '],"must_not": [' + mustnot_array + '] ,"filter":[{ "range": { "@timestamp":{ "gte": "now-' + time_period + '" }}}]}}}')
+    elif must_array :
+        jquery = json.loads('{  "query": { "bool": {"must": [' + must_array + '],"filter":[{ "range": { "@timestamp":{ "gte": "now-' + time_period + '" }}}]}}}')
+    elif mustnot_array :
+        jquery = json.loads('{  "query": { "bool": {"must_not": [' + mustnot_array + '],"filter":[{ "range": { "@timestamp":{ "gte": "now-' + time_period + '" }}}]}}}')
+    
     headers = {'Accept': 'application/json', 'Content-type': 'application/json'}
-    #print(jquery)
+   
     
     if cfg['elasticsearch']['ssl'] == True: 
         queryURL = "https://"  + cfg["elasticsearch"]["host"]  + ":" + cfg["elasticsearch"]["port"] + "/" + index_pattern + "/_search"
@@ -52,7 +61,7 @@ def alertQuery(index_pattern, field, term,time_period, negate) :
         
     return json.loads(response.text)
     
-# Dissescts the field names. Will be used later on to print specific values.    
+# Disescts the field names. Will be used later on to print specific values.    
 def getFieldNames(field) :
     if '.' not in field :
         return field
@@ -65,7 +74,6 @@ def getFieldNames(field) :
         return parsedField
             
 def sendEvents(host, port, username, recipient, password, message) :
-    #sender = socket.getfqdn()
     
     sent_from = username
     to = recipient
@@ -105,33 +113,81 @@ def main():
     
     # This is where we check all configured alerts.
     for alert in cfg['alerts'] :
-        json_response = alertQuery(alert['index_pattern'],alert['field'],alert['term'],alert['time_period'], alert['negate'])
+        
+        if 'must' in alert and 'must_not' in alert :
+            json_response = alertQuery(alert['index_pattern'],alert['time_period'],alert['must'],alert['must_not'])
+        if 'must' in alert :
+            json_response = alertQuery(alert['index_pattern'],alert['time_period'],alert['must'],None)    
+        if 'must_not' in alert :
+            json_response = alertQuery(alert['index_pattern'],alert['time_period'],None,alert['must_not'])    
               
+        docids = []
         if json_response['hits']['total']['value'] != 0 :
             for hit in json_response['hits']['hits'] :
-    
-                alertValue = parse(alert['field']).find(hit['_source'])
                 
-                #alertOut = "Alert matched: " + str(alertValue[0].value) + "\n" + "Time: " + hit['_source']['@timestamp'] + "\n\n" + "Full message: \n\n" + json.dumps(hit['_source'], indent=2)
-                alertOut = "Alert matched: " + str(alertValue[0].value) + "\n" + "Time: " + hit['_source']['@timestamp'] + "\n\n" + "Full message: \n\n" + json.dumps(hit['_source'])
                 
-                if cfg['output']['log']['enabled'] :
-                    logger.debug("Writing to log...")
-                    logger.debug(alertOut)
+                if hit['_id'] not in docids :
+                    alertValue = str(alert['must'])
                     
-                if cfg['output']['console']['enabled'] :
-                    print(alertOut)
-                
-                if cfg['output']['smtp']['enabled'] :
-                    sendEvents(cfg['output']['smtp']['host'] , cfg['output']['smtp']['port'] , cfg['output']['smtp']['username'], alert['recipient'], cfg['output']['smtp']['password'] , alertOut)        
+                    alertOut = "Alert matched: " + str(alertValue) + "\n" + "Time: " + hit['_source']['@timestamp'] + "\n\n" + "Full message: \n\n" + json.dumps(hit['_source'], indent=2)
+                    
+                    if cfg['output']['log']['enabled'] :
+                        logger.debug("Writing to log...")
+                        logger.debug(alertOut)
+                        
+                    if cfg['output']['console']['enabled'] :
+                        print(alertOut)
+                    
+                    if cfg['output']['smtp']['enabled'] :
+                        sendEvents(cfg['output']['smtp']['host'] , cfg['output']['smtp']['port'] , cfg['output']['smtp']['username'], alert['recipient'], cfg['output']['smtp']['password'] , alertOut)        
+    
+                    docids.append(hit['_id'])
 
         else:
-             logger.debug(timestamp.strftime("%m/%d/%y %H:%M:%S") + ": No results returned for " + alert['name'])
+            logger.debug(timestamp.strftime("%m/%d/%y %H:%M:%S") + ": No results returned for " + alert['name'])
              
-             if cfg['output']['console']['enabled'] :
+            if cfg['output']['console']['enabled'] :
                 print(timestamp.strftime("%m/%d/%y %H:%M:%S") + ": No results returned for " + alert['name'])
     logger.debug("Run finished")
             
 if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(description='Export Elastic query results to CSV or JSON via email or local file')
+    parser.add_argument('--config', dest='configfile', default='config/test2.yml')
+    parser.add_argument('-E', dest='E', help="YAML configuration overrides", nargs='*', action='append')
+    
+    args = parser.parse_args()
+        
+    # Import the alerts configuration
+    file = open(args.configfile,'r')
+    cfg = yaml.load(file, Loader=yaml.FullLoader)
+    
+    if args.E :
+        for override in args.E:
+            ############################################################
+            # Split by the delimiter, making sure to split once only
+            # to prevent splitting when the delimiter appears in the value
+            key, value = override[0].split("=", 1)
+            
+            # Break the dot-joined key into parts that form a path
+            key_parts = key.split(".")
+            parent_key = key_parts[0]
+            
+            # The last part is required to update the dictionary
+            last_part = key_parts.pop()
+            
+            # Traverse the dictionary using the parts
+            current = copy.deepcopy(cfg)
+            while key_parts:
+                current = current[key_parts.pop(0)]
+        
+            # Update the value
+            current[last_part] = value
+            
+            cfg[parent_key] = current 
+            ############################################################
+        
+    
+    
     main()
 
